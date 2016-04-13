ac514a9807c38e12d027ef6ebe8dc448f7fd1da6
Roman Golyuk <roman.golyuk@gmail.com>
2016-04-13 19:24:22 +0300

    Fixed issue when commissioner could change admin user role

--- src/main/java/org/registrator/community/service/impl/RoleServiceImpl.java
+++ src/main/java/org/registrator/community/service/impl/RoleServiceImpl.java
@@ -1,10 +1,16 @@
 package org.registrator.community.service.impl;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.registrator.community.dao.RoleRepository;
 import org.registrator.community.entity.Role;
+import org.registrator.community.enumeration.RoleType;
 import org.registrator.community.service.RoleService;
+import org.registrator.community.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,6 +20,9 @@ public class RoleServiceImpl implements RoleService{
 
 	@Autowired
 	RoleRepository roleRepository;
+
+	@Autowired
+	UserService userService;
 	
 	@Transactional
 	@Override
@@ -21,4 +30,41 @@ public class RoleServiceImpl implements RoleService{
 		return roleRepository.findAll();
 	}
 
+	/**
+	 * Returns roles which current user can assign to users with specified RoleType
+	 * @param currentRole users role for which changing roles is applied
+	 */
+	@Override
+	public List<Role> getAvailableRolesToAssign(RoleType currentRole) {
+        /*
+        so we have redundant Role entity, it would be much simpler with @Enumerable RoleType on User
+        instead of separate entity Role
+        couldn't fix that right now
+        */
+
+		Role userRole = userService.getLoggedUser().getRole();
+		List<RoleType> availableRoleType = Collections.emptyList();
+		if (userRole.getType() == RoleType.ADMIN) {
+			// admin can change all roles
+			availableRoleType = Arrays.asList(RoleType.values());
+		} else if (userRole.getType() == RoleType.COMMISSIONER) {
+			// commissioner can't change admin role
+			if (currentRole == RoleType.ADMIN) {
+				availableRoleType = Collections.singletonList(RoleType.ADMIN);
+			} else {
+                availableRoleType = Arrays.stream(RoleType.values())
+                        .filter(roleType -> roleType != RoleType.ADMIN)
+                        .collect(Collectors.toList());
+            }
+		}
+
+        List<Role> result = new ArrayList<>();
+        for (Role role : getAllRole()) {
+            if (availableRoleType.contains(role.getType())) {
+                result.add(role);
+            }
+        }
+
+        return result;
+	}
 }
