7aa655ea7c3d00a4a5a203d13361f1d25c7a4c09
Roman Golyuk <roman.golyuk@gmail.com>
2016-04-22 12:01:32 +0300

    Deleted class HttpUtils

--- src/main/java/org/registrator/community/controller/ResourceController.java
+++ src/main/java/org/registrator/community/controller/ResourceController.java
@@ -10,7 +10,6 @@ import org.registrator.community.dto.json.SearchResultJson;
 import org.registrator.community.entity.*;
 import org.registrator.community.exceptions.ResourceEntityNotFound;
 import org.registrator.community.service.*;
-import org.registrator.community.utils.HttpUtils;
 import org.registrator.community.validator.ResourceDTOValidator;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -147,7 +146,7 @@ public class ResourceController {
      */
     @ExceptionHandler(ResourceEntityNotFound.class)
     public ModelAndView handleResourceEntityNotFound(HttpServletRequest request, ResourceEntityNotFound exception) {
-        logger.warn("Request: " + HttpUtils.getFullRequestURL(request)
+        logger.warn("Request: " + request.getRequestURL()
                 + " raised " + exception.getClass().getName());
 
         ModelAndView mav = new ModelAndView();
