79b085ccb1bf5146dee02a87db23bc9bfac543b4
Roman Holiuk <roman.golyuk@gmail.com>
2016-04-23 13:36:43 +0300

    Experimental changes with websocket

---
+++ src/main/java/org/registrator/community/controller/ChatController.java
@@ -0,0 +1,14 @@
+package org.registrator.community.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ChatController {
+
+    @RequestMapping("/chat")
+    public String chat() {
+        return "chat";
+    }
+}
