6d68ab34224f5f164e3b231d27882505db75b103
Roman Golyuk <roman.golyuk@gmail.com>
2016-04-26 16:23:07 +0300

    Implemented admin notify when email was not sent

---
+++ src/main/java/org/registrator/community/websocket/MessageCommand.java
@@ -0,0 +1,9 @@
+package org.registrator.community.websocket;
+
+/** Command that need to be applied with message, either on client or server
+ *
+ */
+public enum MessageCommand {
+    SHOW,
+    CLOSE
+}
